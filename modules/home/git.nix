70282e7caaddff4cc9052a35be04f5a615fce238
Mayank Anand <131797761+techanand8@users.noreply.github.com>
2026-05-21 10:55:34 +0530

    System Update - Generation  - 2026-05-21 10:55

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,27 +1,30 @@
 { config, pkgs, vars, ... }:
 
 {
-  # Professional Git Configuration
+  # Professional Git Configuration (Modern Schema)
   programs.git = {
     enable = true;
-    userName = vars.fullName;
-    userEmail = vars.email;
 
-    extraConfig = {
+    settings = {
+      user = {
+        name = vars.fullName;
+        email = vars.email;
+      };
+
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
       pull.rebase = true;
       core.editor = "nvim";
-    };
 
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      cm = "commit";
-      ps = "push";
-      pl = "pull";
-      lg = "log --oneline --graph --decorate";
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        cm = "commit";
+        ps = "push";
+        pl = "pull";
+        lg = "log --oneline --graph --decorate";
+      };
     };
   };
 }
--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -7,38 +7,36 @@
     enableDefaultConfig = false; # Cleanest approach: only use your custom hardened settings
 
     # Global Settings for all hosts (*)
-    matchBlocks = {
+    settings = {
       "*" = {
         # Identity Persistence
-        extraOptions = {
-          AddKeysToAgent = "yes";
-          IdentityAgent = "none";
-          VerifyHostKeyDNS = "yes";
-          PasswordAuthentication = "no";
-          # Professional Encryption Standards
-          Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com";
-          MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com";
-        };
+        AddKeysToAgent = "yes";
+        IdentityAgent = "none";
+        VerifyHostKeyDNS = "yes";
+        PasswordAuthentication = "no";
 
-        kexAlgorithms = [ "curve25519-sha256" "curve25519-sha256@libssh.org" ];
+        # Professional Encryption Standards
+        Ciphers = "chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com";
+        MACs = "hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com";
+        KexAlgorithms = [ "curve25519-sha256" "curve25519-sha256@libssh.org" ];
 
         # SECURITY HARDENING:
         # Hide the identities of servers you connect to
-        hashKnownHosts = true;
+        HashKnownHosts = "yes";
 
         # Prevent man-in-the-middle attacks by strictly checking keys
-        controlMaster = "auto";
-        controlPath = "~/.ssh/master-%r@%h:%p";
+        ControlMaster = "auto";
+        ControlPath = "~/.ssh/master-%r@%h:%p";
 
         # Enforce strong cryptography (God Mode Encryption)
-        forwardAgent = false;
+        ForwardAgent = "no";
       };
 
       # Specific identity settings for GitHub
       "github.com" = {
-        hostname = "github.com";
-        user = "git";
-        identityFile = "~/.ssh/id_ed25519";
+        HostName = "github.com";
+        User = "git";
+        IdentityFile = [ "~/.ssh/id_ed25519" ];
       };
     };
   };
