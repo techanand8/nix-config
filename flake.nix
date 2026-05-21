0754752eb73de18fac10dded02968b52a86f3fe6
Mayank Anand <131797761+techanand8@users.noreply.github.com>
2026-05-21 10:57:31 +0530

    System Update - Generation  - 2026-05-21 10:57

--- flake.nix
+++ flake.nix
@@ -46,6 +46,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "bak";
             home-manager.extraSpecialArgs = { inherit inputs vars; };
             home-manager.users."${vars.username}" = {
               imports = [
