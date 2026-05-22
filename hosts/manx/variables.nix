06e6a20c3f59e2e79b004988fff8351f2dc81d7d
Mayank Anand <131797761+techanand8@users.noreply.github.com>
2026-05-22 10:01:24 +0530

    System Update - Generation 66 - 2026-05-22 10:01

---
+++ hosts/manx/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Personal Info
+  username = "mayank-anand";
+  fullName = "Mayank Anand";
+  email = "131797761+techanand8@users.noreply.github.com";
+
+  # System Settings
+  hostname = "MANX";
+  timezone = "Asia/Kolkata";
+  locale = "en_IN";
+
+  # Hardware / Performance
+  cpuType = "amd";
+  gpuType = "amd";
+  stateVersion = "26.05";
+}
