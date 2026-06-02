23bd998c7c40397c937c695de331b57ca14d44f7
Mayank Anand <131797761+techanand8@users.noreply.github.com>
2026-06-02 20:07:35 +0530

    System Update: 2026-06-02 20:07

--- modules/system/apps.nix
+++ modules/system/apps.nix
@@ -6,6 +6,9 @@
     # Zen Browser (natively packaged via trusted flake input)
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
 
+    # Chromium Browser (With high-performance hardware acceleration & account support)
+    pkgs.chromium
+
     # Add future additional system-level packages here:
     # pkgs.some-app
     pkgs.obs-studio
